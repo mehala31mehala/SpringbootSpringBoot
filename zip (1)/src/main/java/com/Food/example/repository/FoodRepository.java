b60cae1fafae5245b0ef58ba0a45a603e6661b12
unknown <keerthikarnan430@gmail.com>
2023-02-06 05:28:44 -0500

    springboot

--- zip (1)/src/main/java/com/Food/example/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Food.example.repository;
-
-import com.Food.example.entity.FoodCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoodRepository extends JpaRepository<FoodCompany,Integer> {
-}
